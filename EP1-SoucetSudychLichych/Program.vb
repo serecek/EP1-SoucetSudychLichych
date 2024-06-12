e2e70c1da7cc751e7e8f22f2831632cbb56c01a9
Jan Piňos <Pinos.Jan@stud.vassboskovice.cz>
2024-06-12 10:37:44 +0200

    soucetsudychlichych

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -1,7 +1,27 @@
 Imports System
 
-Module Program
-  Sub Main(args As String())
-    Console.WriteLine("Hello World!")
-  End Sub
+Module Module1
+
+    Sub Main()
+        Dim pocetCisel As Integer
+        Dim cisla(pocetCisel - 1) As Integer
+
+        Console.WriteLine("Zadejte poèet èísel, které chcete zadat:")
+        pocetCisel = Integer.Parse(Console.ReadLine())
+
+        For i As Integer = 0 To pocetCisel - 1
+            Console.WriteLine("Zadejte èíslo {0}:", i + 1)
+            cisla(i) = Integer.Parse(Console.ReadLine())
+        Next
+
+        Console.WriteLine("Zadaná èísla jsou:")
+
+        For Each cislo As Integer In cisla
+            Console.WriteLine(cislo)
+        Next
+        Console.WriteLine("Stisknìte libovolnou klávesu pro ukonèení programu.")
+        Console.ReadKey()
+
+    End Sub
+
 End Module
